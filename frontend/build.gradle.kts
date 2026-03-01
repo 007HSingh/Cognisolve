2740286e5ed0bbb347b7196d7e7686dd00c73e68
Harsh Singh <singhharsh25032008@gmail.com>
2026-03-01 18:47:22 +0530

    chore: versions made compatible

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.jetbrains.kotlin.multiplatform") version "2.1.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
     id("org.jetbrains.compose") version "1.7.3" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
 }
